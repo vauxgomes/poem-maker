1e8aff0d39741ba4d5fce3b12441f982bb90a671
vaux.gomes <vauxsandino@gmail.com>
2021-12-20 17:17:10 -0300

    Media queries added

--- js/main.js
+++ js/main.js
@@ -1,79 +1,111 @@
 // Data
 const data = {
-	sections: [
-		{
-			qid: 1,
-			header: 'Question 1',
-			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
-			options: [
-				{ oid: 1, text: 'Option 1' },
-				{ oid: 2, text: 'Option 2' },
-				{ oid: 3, text: 'Option 3' },
-				{ oid: 4, text: 'Option 4' },
-			],
-		},
+    sections: [
+        {
+            qId: 1,
+            header: "Question 1",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+            options: [
+                { opId: 1, text: "Option 1.1", result: "Teste" },
+                { opId: 2, text: "Option 1.2", result: "Teste" },
+                { opId: 3, text: "Option 1.3", result: "Teste" },
+                { opId: 4, text: "Option 1.4", result: "Teste" },
+            ],
+        },
 
-		{
-			qid: 2,
-			header: 'Question 2',
-			text: 'Accusamus nemo nam quaerat ab quibusdam eius, quidem.',
-			options: [
-				{ oid: 1, text: 'Option 1' },
-				{ oid: 2, text: 'Option 2' },
-				{ oid: 3, text: 'Option 3' },
-				{ oid: 4, text: 'Option 4' },
-			],
-		},
-	],
+        {
+            qId: 2,
+            header: "Question 2",
+            text: "Accusamus nemo nam quaerat ab quibusdam eius, quidem.",
+            options: [
+                { opId: 1, text: "Option 2.1" },
+                { opId: 2, text: "Option 2.2" },
+                { opId: 3, text: "Option 2.3" },
+                { opId: 4, text: "Option 2.4" },
+            ],
+        },
+
+        {
+            qId: 3,
+            header: "Question 2",
+            text: "Accusamus nemo nam quaerat ab quibusdam eius, quidem.",
+            options: [
+                { opId: 1, text: "Option 2.1" },
+                { opId: 2, text: "Option 2.2" },
+                { opId: 3, text: "Option 2.3" },
+                { opId: 4, text: "Option 2.4" },
+            ],
+        },
+
+        {
+            qId: 4,
+            header: "Question 2",
+            text: "Accusamus nemo nam quaerat ab quibusdam eius, quidem.",
+            options: [
+                { opId: 1, text: "Option 2.1" },
+                { opId: 2, text: "Option 2.2" },
+                { opId: 3, text: "Option 2.3" },
+                { opId: 4, text: "Option 2.4" },
+            ],
+        },
+    ],
 };
 
 // Template
-const slideTemplate = document.querySelector('#template').innerHTML;
-let slides = document.querySelector('.slides');
+let slides = document.querySelector(".slides");
 slides.innerHTML = Mustache.render(slideTemplate, data);
 
 // Slide actions
-let slider = document.querySelector('.slider');
-document.querySelectorAll('.slide').forEach((slide, index) => {
-	let dot = document.createElement('span');
-	dot.classList.add('dot');
-	dot.addEventListener('click', (e) => showSlides(index));
+let slider = document.querySelector(".slider");
+document.querySelectorAll(".slide").forEach((slide, index) => {
+    let dot = document.createElement("span");
+    dot.classList.add("dot");
+    dot.addEventListener("click", (e) => showSlides(index));
 
-	slider.append(dot);
+    slider.append(dot);
 });
 
-document.querySelectorAll('.verse').forEach((verse, index) => {
-	verse.addEventListener('click', (e) => showSlides(index));
+document.querySelectorAll(".verse").forEach((verse, index) => {
+    verse.addEventListener("click", (e) => showSlides(index));
 });
 
 let slideIndex = 0;
 showSlides(slideIndex);
 
 function showSlides(n) {
-	var slides = document.querySelectorAll('.slide');
-	var dots = document.querySelectorAll('.dot');
-	var verses = document.querySelectorAll('.verse');
+    var slides = document.querySelectorAll(".slide");
+    var dots = document.querySelectorAll(".dot");
+    var verses = document.querySelectorAll(".verse");
 
-	// Safety
-	if (n >= slides.length) {
-		n = 0;
-	} else if (n < 0) {
-		n = slides.length - 1;
-	}
+    // Safety
+    if (n >= slides.length) {
+        n = 0;
+    } else if (n < 0) {
+        n = slides.length - 1;
+    }
 
-	slides.forEach((item) => {
-		item.style.display = 'none';
-	});
+    slides.forEach((item) => {
+        item.style.display = "none";
+    });
 
-	dots.forEach((item) => {
-		item.classList.remove('active');
-	});
+    dots.forEach((item) => {
+        item.classList.remove("active");
+    });
 
-	verses.forEach((item) => {
-		item.classList.remove('active');
-	});
+    verses.forEach((item) => {
+        item.classList.remove("active");
+    });
 
-	slides[n].style.display = 'block';
-	dots[n].classList.add('active');
-	verses[n].classList.add('active');
+    slides[n].style.display = "block";
+    dots[n].classList.add("active");
+    verses[n].classList.add("active");
+
+    slideIndex = n;
+}
+
+function prevSlide() {
+    showSlides(slideIndex - 1);
+}
+function nextSlide() {
+    showSlides(slideIndex + 1);
 }
--- js/templates/slide.js
+++ js/templates/slide.js
@@ -1,20 +1,16 @@
-<div class="slide fade">
-	<span class="title">{{ header }}</span>
-	<section>
-		<p>{{ text }}</p>
+let slideTemplate = `
+{{#sections}}
+	<div class="slide fade">
+		<span class="title">{{ header }}</span>
+		<section>
+			<p>{{text}}</p>
 
-		<div class="options">
-			<input value="0" type="radio" name="q1" id="q1-1" />
-			<label for="q1-1">Option 1</label>
-
-			<input value="1" type="radio" name="q1" id="q1-2" />
-			<label for="q1-2">Option 2</label>
-
-			<input value="2" type="radio" name="q1" id="q1-3" />
-			<label for="q1-3">Option 3</label>
-
-			<input value="3" type="radio" name="q1" id="q1-4" />
-			<label for="q1-4">Option 4</label>
-		</div>
-	</section>
-</div>;
+			<div class="options">
+				{{#options}}
+						<input value="0" type="radio" name="qt-{{qId}}" id="qt-{{qId}}-{{opId}}" />
+						<label for="qt-{{qId}}-{{opId}}">{{text}}</label>
+				{{/options}}
+			</div>
+		</section>
+	</div>
+{{/sections}}`;
